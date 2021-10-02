026cf6db8f46ba15733d0ad7971d55dbe334c0be
Yegor <egoryefim@gmail.com>
2021-10-02 20:10:01 +0300

    added search by movie name functionality

--- src/views/MoviesPage/MoviesPage.jsx
+++ src/views/MoviesPage/MoviesPage.jsx
@@ -1,9 +1,49 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import * as moviesAPI from '../../services/movies-api';
+import '../HomePage/HomePage.css'
+
+
 export default function MoviesPage() {
+    const [movie, setMovie] = useState(null);
+    const [movies, setMovies] = useState(null);
+    console.log(movies);
+
+    const handleChange = e => {
+        console.log(e.target.value);
+        setMovie(e.target.value);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        moviesAPI.searcMovie(movie).then(setMovies)
+    }
+    
+    
+    
+    
     return (
-        <form action="">
-            <label htmlFor="">
-                <input type="text" />
-            </label>
-        </form>
-    )
+        <>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="">
+                    <input type="text"
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <button>Search</button>
+            </form>
+
+            <ul className="MovieGallery">
+            {movies &&
+                
+                movies.results.map(movie => <li key={movie.id} className="MovieGalleryItem">
+                    <Link to={`movies/${movie.id}`}>
+                        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" className="MovieGalleryItem-image"/>
+                    </Link>
+                    
+                </li>)}
+        </ul>
+        </>
+    );
 }

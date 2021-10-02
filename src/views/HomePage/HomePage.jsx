daf0f8d7cc390e0ef643398e7cfe8be1e531d805
Yegor <egoryefim@gmail.com>
2021-10-02 20:09:38 +0300

    minor changes

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import * as moviesAPI from '../../services/movies-api'
+import * as moviesAPI from '../../services/movies-api';
 import "./HomePage.css"
 
 
